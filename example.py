49ca4195a8bb55d39d62bc368837def4255c11a3
charkster <31560568+charkster@users.noreply.github.com>
2025-09-26 19:06:01 -0700

    Update example.py

--- example.py
+++ example.py
@@ -2,13 +2,13 @@
 
 import time
 import pyvisa
-from tektronics_afg3000 import tektronics_afg3000
+from tektronix_afg3102 import tektronics_afg3102
 
 rm = pyvisa.ResourceManager()
 tekafg3102 = rm.open_resource('USB::0x000::0x000::C000000::INSTR')
-tektronics_afg3000 = tektronics_afg3000(pyvisa_instr=tekafg3102)
+tektronics_afg3102 = tektronics_afg3102(pyvisa_instr=tekafg3102)
 
-current_setting_list = tektronics_afg3000.get_unique_scpi_list()
+current_setting_list = tektronics_afg3102.get_unique_scpi_list()
 
 tekafg3102.write('*RST')
 time.sleep(2)
